f168f01f6e8f608721d31eb331f7a56a1dea6ba7
J. M. Hinckley <jhinckley@vbisd.org>
2024-02-22 14:24:44 -0500

    Modified the output and added a comment

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 def main():
-    print ("This is the original code.")
+    # This comment is an added line in the file.
+    print ("This has been modified from the original code.")
 
 if __name__ == "__main__":
     main()
