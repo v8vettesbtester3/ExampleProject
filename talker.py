86c9e3246f1d7eca6391813bf6bfd21e23bcad60
J. M. Hinckley <jhinckley@vbisd.org>
2024-02-22 15:19:07 -0500

    Modified the Talker class

--- talker.py
+++ talker.py
@@ -6,9 +6,13 @@ class Talker(object):
     def speak(self, phrase, loudly = False):
         if loudly:
             # speaking loudly using uppercase
-            self.msg = phrase.upper()
+            self.msg = phrase
+            self.yell(self.msg)
         else:
             # speaking not loudly, normal case
             self.msg = phrase
         print(self.msg)
+
+    def yell(self, phrase):
+        print(phrase.upper())
 
