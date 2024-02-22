c31b364cecc7159f7bfa7a2f67c3ecd15d9e1817
J. M. Hinckley <jhinckley@vbisd.org>
2024-02-22 15:31:55 -0500

    Bug fix in Tracker.py

--- talker.py
+++ talker.py
@@ -11,7 +11,7 @@ class Talker(object):
         else:
             # speaking not loudly, normal case
             self.msg = phrase
-        print(self.msg)
+            print(self.msg)
 
     def yell(self, phrase):
         print(phrase.upper())
